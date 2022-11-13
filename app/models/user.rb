097b04f810a6855db21fd384f7ef8d8516838fae
chan <baguschandradp@gmail.com>
2022-11-13 16:57:50 +0800

    add comment controls

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_person_name
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates_uniqueness_of :username
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
   authenticate :user, ->(u) { u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
-  member do
-    post :retweet
-  end
 
   devise_for :users
-  resources :tweets, except: %i[edit update]
+  resources :tweets, except: %i[edit update] do
+    resources :comments, only: %i[create destroy]
+    member do
+      post :retweet
+    end
+  end
+  resources :like, only: :create
   resources :profiles
   root to: 'tweets#index'
 end
